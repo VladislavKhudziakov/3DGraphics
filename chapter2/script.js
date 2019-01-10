30ab7b409e3baf822d06b586cf1cc7709f9357c3
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-01-10 10:50:22 +0300

    chapter 2: page 66

--- chapter2/script.js
+++ chapter2/script.js
@@ -1,27 +1,38 @@
-const VSHADER_SOURCE =`void main()
+const VSHADER_SOURCE =`
+attribute vec4 a_Position;
+void main()
 {
-  gl_Position = vec4(0.0, 0.0, 0.0, 1.0); 
+  gl_Position = a_Position; 
   gl_PointSize = 10.0; 
-};`
+}`
 
 const FSHADER_SOURCE =`void main() 
 {
-  gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0); 
-};`
+  gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0); 
+}`
+
+
 
 const canvas = document.getElementById('example');
 const gl = canvas.getContext('webgl');
 
-const vshader = gl.createShader(gl.VERTEX_SHADER);
-const fshader = gl.createShader(gl.FRAGMENT_SHADER);
+initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
-gl.shaderSource(vshader, VSHADER_SOURCE);
-gl.shaderSource(fshader, FSHADER_SOURCE);
-gl.compileShader(vshader);
-gl.compileShader(fshader);
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-gl.drawArrays(gl.POINTS, 0, 1);
+let a_Position = gl.getAttribLocation(gl.program, 'a_Position');
+
+let timer = setInterval((() => {
+  let value = -1.0;
+  return () => {
+    value += 0.01;
+    if(value >= 1.0) {
+      value = -1.0;
+    }
+    gl.vertexAttrib4f(a_Position, value, 0.0, 0.0, 1.0);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.drawArrays(gl.POINTS, 0, 1);
+  }
+})(), 10);
 
 
 
