ca9a3571b149a02261a301a71a9bf216ac5a1286
Vladislav <super.tawer.822@gmail.com>
2019-01-13 20:10:29 +0300

    chapter3: page 132

--- chapter3/script.js
+++ chapter3/script.js
@@ -1,10 +1,12 @@
 const VSHADER_SOURCE =`
 attribute vec4 a_Position;
-//attribute float a_PointSize;
+uniform vec4 u_Translation;
+uniform float u_SinB, u_CosB;
+uniform float u_Scale;
+uniform mat4 u_xFormMatrix, u_ScaleMatrix, u_TranslateMatrix;
 void main()
 {
-  gl_Position = a_Position; 
-  //gl_PointSize = a_PointSize; 
+  gl_Position = a_Position * u_ScaleMatrix * u_xFormMatrix * u_TranslateMatrix;
 }`;
 
 const FSHADER_SOURCE =`
@@ -23,10 +25,70 @@ gl.clear(gl.COLOR_BUFFER_BIT);
 
 initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
-const a_PointSize = gl.getAttribLocation(gl.program, 'a_PointSize');
 const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-gl.vertexAttrib1f(a_PointSize, 10.0);
 gl.uniform4f(u_FragColor, 0.0, 1.0, 0.0, 1.0);
+
+const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
+gl.uniform4f(u_Translation, 0.0, 0.0, 0.0, 0.0);
+
+const angle = 0;
+const u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
+const u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
+gl.uniform1f(u_SinB, Math.sin(angle));
+gl.uniform1f(u_CosB, Math.cos(angle));
+
+const scaleValue = 1;
+const u_Scale = gl.getUniformLocation(gl.program, 'u_Scale');
+gl.uniform1f(u_Scale, scaleValue);
+
+const cosB = Math.cos(angle);
+const sinB = Math.sin(angle);
+
+const xFormMatrix = new Float32Array([
+   cosB, sinB, 0.0, 0.0,
+  -sinB, cosB, 0.0, 0.0,
+    0.0,  0.0, 1.0, 0.0,
+    0.0,  0.0, 0.0, 1.0
+]);
+
+const u_xFormMatrix = gl.getUniformLocation(gl.program, 'u_xFormMatrix');
+gl.uniformMatrix4fv(u_xFormMatrix, false, xFormMatrix);
+
+const xSc = 1.0;
+const ySc = 1.0;
+
+const xScaleMatrix = new Float32Array([
+  xSc, 0.0, 0.0, 0.0,
+  0.0, ySc, 0.0, 0.0,
+  0.0, 0.0, 0.0, 0.0,
+  0.0, 0.0, 0.0, 1.0
+]);
+
+const u_ScaleMatrix = gl.getUniformLocation(gl.program, 'u_ScaleMatrix');
+gl.uniformMatrix4fv(u_ScaleMatrix, false, xScaleMatrix);
+
+const xT = 0.0;
+const yT = 0.0;
+const zT = 0.0;
+
+xTranslateMatrix = new Float32Array([
+  1.0, 0.0, 0.0, xT,
+  0.0, 1.0, 0.0, yT,
+  0.0, 0.0, 1.0, zT,
+  0.0, 0.0, 0.0, 1.0
+]);
+
+const u_TranslateMatrix = gl.getUniformLocation(gl.program, 'u_TranslateMatrix');
+gl.uniformMatrix4fv(u_TranslateMatrix, false, xTranslateMatrix);
+
+/**
+* [
+*   tr_x.x, tr_y.x, tr_z.x, tr_p.x,
+*   tr_x.y, tr_y.y, tr_z.y, tr_p.y,
+*   tr_x.z, tr_y.z, tr_z.z, tr_p.z,
+*      0.0,    0.0,    0.0,    1.0
+* ]
+*/
 
 
 const n = initVertexBuffers(gl);
@@ -39,11 +101,8 @@ gl.drawArrays(gl.TRIANGLES, 0, n);
 
 function initVertexBuffers(gl) {
   const vertices = new Float32Array([
-    -0.5, 0.5, 0.0, 
+    0.0, 0.5, 0.0, 
     -0.5, -0.5, 0.0,
-    0.5, -0.5, 0.0,
-    -0.5, 0.5, 0.0, 
-    0.5, 0.5, 0.0, 
     0.5, -0.5, 0.0,
   ]);
 
