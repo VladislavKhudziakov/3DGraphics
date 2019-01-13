a507604fe8bcee5b46079507ea2c7b9b07e9c8c7
Vladislav <super.tawer.822@gmail.com>
2019-01-13 21:43:22 +0300

    chapter3: final App: TODO: INTERFACE, TRANSFORMATION

--- chapter3/script.js
+++ chapter3/script.js
@@ -1,12 +1,9 @@
 const VSHADER_SOURCE =`
 attribute vec4 a_Position;
-uniform vec4 u_Translation;
-uniform float u_SinB, u_CosB;
-uniform float u_Scale;
-uniform mat4 u_xFormMatrix, u_ScaleMatrix, u_TranslateMatrix;
+uniform mat4 u_RotateMatrix, u_ScaleMatrix, u_TranslateMatrix;
 void main()
 {
-  gl_Position = a_Position * u_ScaleMatrix * u_xFormMatrix * u_TranslateMatrix;
+  gl_Position = a_Position * u_ScaleMatrix * u_RotateMatrix * u_TranslateMatrix;
 }`;
 
 const FSHADER_SOURCE =`
@@ -20,114 +17,141 @@ void main()
 const canvas = document.getElementById('chapter3');
 const gl = canvas.getContext('webgl');
 
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
-
-initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
-
-const u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
-gl.uniform4f(u_FragColor, 0.0, 1.0, 0.0, 1.0);
-
-const u_Translation = gl.getUniformLocation(gl.program, 'u_Translation');
-gl.uniform4f(u_Translation, 0.0, 0.0, 0.0, 0.0);
-
-const angle = 0;
-const u_SinB = gl.getUniformLocation(gl.program, 'u_SinB');
-const u_CosB = gl.getUniformLocation(gl.program, 'u_CosB');
-gl.uniform1f(u_SinB, Math.sin(angle));
-gl.uniform1f(u_CosB, Math.cos(angle));
-
-const scaleValue = 1;
-const u_Scale = gl.getUniformLocation(gl.program, 'u_Scale');
-gl.uniform1f(u_Scale, scaleValue);
-
-const cosB = Math.cos(angle);
-const sinB = Math.sin(angle);
-
-const xFormMatrix = new Float32Array([
-   cosB, sinB, 0.0, 0.0,
-  -sinB, cosB, 0.0, 0.0,
-    0.0,  0.0, 1.0, 0.0,
-    0.0,  0.0, 0.0, 1.0
-]);
-
-const u_xFormMatrix = gl.getUniformLocation(gl.program, 'u_xFormMatrix');
-gl.uniformMatrix4fv(u_xFormMatrix, false, xFormMatrix);
-
-const xSc = 1.0;
-const ySc = 1.0;
-
-const xScaleMatrix = new Float32Array([
-  xSc, 0.0, 0.0, 0.0,
-  0.0, ySc, 0.0, 0.0,
-  0.0, 0.0, 0.0, 0.0,
-  0.0, 0.0, 0.0, 1.0
-]);
-
-const u_ScaleMatrix = gl.getUniformLocation(gl.program, 'u_ScaleMatrix');
-gl.uniformMatrix4fv(u_ScaleMatrix, false, xScaleMatrix);
-
-const xT = 0.0;
-const yT = 0.0;
-const zT = 0.0;
-
-xTranslateMatrix = new Float32Array([
-  1.0, 0.0, 0.0, xT,
-  0.0, 1.0, 0.0, yT,
-  0.0, 0.0, 1.0, zT,
-  0.0, 0.0, 0.0, 1.0
-]);
-
-const u_TranslateMatrix = gl.getUniformLocation(gl.program, 'u_TranslateMatrix');
-gl.uniformMatrix4fv(u_TranslateMatrix, false, xTranslateMatrix);
-
-/**
-* [
-*   tr_x.x, tr_y.x, tr_z.x, tr_p.x,
-*   tr_x.y, tr_y.y, tr_z.y, tr_p.y,
-*   tr_x.z, tr_y.z, tr_z.z, tr_p.z,
-*      0.0,    0.0,    0.0,    1.0
-* ]
-*/
+const v = [
+  0.0, 0.5, 0.0, 
+  -0.5, -0.5, 0.0,
+  0.5, -0.5, 0.0,
+];
 
 
-const n = initVertexBuffers(gl);
+class App {
 
-if (n < 0) {
-  console.log('Failed to set the positions of the vertices');
-}
+  constructor(canvas, gl, init, vShader, fShader, vertArr) {
+    this._gl = gl;
+    this._canval = canvas;
 
-gl.drawArrays(gl.TRIANGLES, 0, n);
+    init(gl, vShader, fShader);
 
-function initVertexBuffers(gl) {
-  const vertices = new Float32Array([
-    0.0, 0.5, 0.0, 
-    -0.5, -0.5, 0.0,
-    0.5, -0.5, 0.0,
-  ]);
+    this._vertices = new Float32Array(vertArr);
+    this.vertexCount = this._initVertexBuffer();
+    
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-  const n = vertices.length / 3; // Число вершин
-   // Создать буферный объект
-  const vertexBuffer = gl.createBuffer();
-  if (!vertexBuffer) {
-    console.log('Failed to create the buffer object');
-    return -1;
+    this.u_FragColor = gl.getUniformLocation(gl.program, 'u_FragColor');
+    gl.uniform4f(this.u_FragColor, 0.0, 1.0, 0.0, 1.0);
+
+    this.u_TranslateMatrix = gl.getUniformLocation(gl.program, 'u_TranslateMatrix');
+    this.u_ScaleMatrix = gl.getUniformLocation(gl.program, 'u_ScaleMatrix');
+    this.u_RotateMatrix = gl.getUniformLocation(gl.program, 'u_RotateMatrix');
+
+    const defTransform = new Transformation().matrix;
+
+    gl.uniformMatrix4fv(this.u_TranslateMatrix, false, defTransform);
+    gl.uniformMatrix4fv(this.u_ScaleMatrix, false, defTransform);
+    gl.uniformMatrix4fv(this.u_RotateMatrix, false, defTransform);
   }
-  // Определить тип буферного объекта
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  // Записать данные в буферный объект
-  gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  // Сохранить ссылку на буферный объект в переменной a_Position
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, false, 0, 0);
-  // Разрешить присваивание переменной a_Position
-  gl.enableVertexAttribArray(a_Position);
-  return n;
 
-  /*
-    [
-      x, y, z, w,
-      x, y, z, w,
-    ]
-  */
+  setVertices(arr) {
+    this._vertices = new Float32Array(arr);
+  }
+
+  _initVertexBuffer() {
+    const vertCnt = this._vertices.length;
+
+    this.vertexBuffer = gl.createBuffer();
+
+    if (!this.vertexBuffer) {
+      console.log('Failed to create the buffer object');
+      return -1;
+    }
+
+    this._gl.bindBuffer(this._gl.ARRAY_BUFFER, this.vertexBuffer);
+    this._gl.bufferData(this._gl.ARRAY_BUFFER, this._vertices, this._gl.STATIC_DRAW);
+
+    this.a_Position = this._gl.getAttribLocation(this._gl.program, 'a_Position');
+
+    this._gl.vertexAttribPointer(this.a_Position, 3, this._gl.FLOAT, false, 0, 0);
+    this._gl.enableVertexAttribArray(this.a_Position);
+    return vertCnt / 3;
+  };
+
+  draw() {
+    if (this.vertexCount === -1) {
+      return new Error('Error: cannot draw');
+    }
+    this._gl.drawArrays(this._gl.TRIANGLES, 0, this.vertexCount);
+  }
+
+  transform(translation, rotation, scaling) {
+    
+    if(translation) {
+      this.u_TranslateMatrix = translation;
+    }
+
+    if(rotation) {
+      this.u_RotateMatrix = rotation;
+    }
+
+    if(scaling) {
+      this.u_ScaleMatrix = scaling;
+    }
+  }
 }
+
+
+class Transformation {
+  constructor() {
+    this.matrix = new Float32Array([
+      1.0, 0.0, 0.0, 0.0,
+      0.0, 1.0, 0.0, 0.0,
+      0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.0, 1.0
+    ]);
+  }
+
+  translate(x, y) {
+    return new Float32Array([
+      1.0, 0.0, 0.0, x.toFixed(1),
+      0.0, 1.0, 0.0, y.toFixed(1),
+      0.0, 0.0, 1.0, 0.0,
+      0.0, 0.0, 0.0, 1.0
+    ]);
+  }
+
+  rotate(deg) {
+    const rad = Math.PI * deg / 180.0;
+
+    const sin = Math.sin(rad);
+    const cos = Math.cos(rad);
+
+    return new Float32Array([
+       cos, sin, 0.0, 0.0,
+      -sin, cos, 0.0, 0.0,
+       0.0, 0.0, 1.0, 0.0,
+       0.0, 0.0, 0.0, 1.0
+    ]);
+  }
+
+  scale(xC, yC) {
+    return new Float32Array([
+        xC, 0.0, 0.0, 0.0,
+       0.0,  yC, 0.0, 0.0,
+       0.0, 0.0, 1.0, 0.0,
+       0.0, 0.0, 0.0, 1.0
+    ]);
+  }
+}
+
+
+class Interface {
+
+};
+
+const app = new App(canvas, gl, initShaders, VSHADER_SOURCE, FSHADER_SOURCE, v);
+app.draw();
+app.transform(
+  new Transformation().translate(0.5, 0.5), 
+  new Transformation().rotate(45), 
+  new Transformation().scale(2.0, 2.0)
+);
