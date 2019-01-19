0d1eaeb4a9c5d973f6fd9f37944f3f45c69174ee
Vladislav <super.tawer.822@gmail.com>
2019-01-20 01:41:49 +0300

    chapter5: final app

--- chapter5/script.js
+++ chapter5/script.js
@@ -21,72 +21,147 @@ void main()
 const canvas = document.getElementById('chapter5');
 const gl = canvas.getContext('webgl');
 
-gl.clearColor(0.0, 0.0, 0.0, 1.0);
-gl.clear(gl.COLOR_BUFFER_BIT);
+const btn = document.getElementById('sendBtn');
+const rGroup = document.getElementsByClassName('rGroup1');
 
-initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
+class App {
+  constructor(canvas, gl, vshader, fshader) {
+    this.gl = gl;
+    this.canvas = canvas;
+    this.vShader = vshader;
+    this.fShader = fshader;
 
-const vertexTexCoord = new Float32Array([
-  -0.5,  0.5, 0.0, 1.0,
-  -0.5, -0.5, 0.0, 0.0,
-   0.5,  0.5, 1.0, 1.0,
-   0.5, -0.5, 1.0, 0.0,
-]);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
 
-const FSIZE = vertexTexCoord.BYTES_PER_ELEMENT;
+    initShaders(gl, vshader, fshader);
 
-const vertCnt = initVertexBuffer();
+    this.vertexTexCoord = new Float32Array([
+      -0.5,  0.5,   0.0, 1.0,
+      -0.5, -0.5,   0.0, 0.0,
+       0.5,  0.5,   1.0, 1.0,
+       0.5, -0.5,   1.0, 0.0,
+    ]);
 
-initTextures(gl, vertCnt);
+    this.FSIZE = this.vertexTexCoord.BYTES_PER_ELEMENT;
 
-gl.drawArrays(gl.TRIANGLE_STRIP, 0, vertCnt);
+    this.vertCnt = this._initVertexBuffer();
+    this.tex1 = gl.createTexture();
+    
+    this.u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    
+    this.tex2 = gl.createTexture();
+    this.tex2 = gl.createTexture();
+    this.tex3 = gl.createTexture();
 
-function initVertexBuffer() {
+    gl.activeTexture(gl.TEXTURE0);
+    gl.bindTexture(gl.TEXTURE_2D, this.tex1);
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, gl.RGBA, 
+      1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, 
+      new Uint8Array([255, 0, 0, 255])
+    );
 
-  const vertCnt = vertexTexCoord.length;
+    gl.activeTexture(gl.TEXTURE1);
+    gl.bindTexture(gl.TEXTURE_2D, this.tex2);
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, gl.RGBA, 
+      1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, 
+      new Uint8Array([0, 255, 0, 255])
+    );
 
-  const vertexBuffer = gl.createBuffer();
+    gl.activeTexture(gl.TEXTURE2);
+    gl.bindTexture(gl.TEXTURE_2D, this.tex3);
+    gl.texImage2D(
+      gl.TEXTURE_2D, 0, gl.RGBA, 
+      1, 1, 0, gl.RGBA, gl.UNSIGNED_BYTE, 
+      new Uint8Array([0, 0, 255, 255])
+    );
+    gl.uniform1i(this.u_Sampler, 0);
+     
+  }
+
+  initTexture() {
+    this.gl.pixelStorei(this.gl.UNPACK_FLIP_Y_WEBGL, 1);
+    this.gl.bindTexture(this.gl.TEXTURE_2D, this.tex1);
+    this.gl.texImage2D(this.gl.TEXTURE_2D, 0, this.gl.RGB,this.gl.RGB, this.gl.UNSIGNED_BYTE, this.image);
+    this.gl.activeTexture(this.gl.TEXTURE0);
+    this.gl.texParameteri(this.gl.TEXTURE_2D, this.gl.TEXTURE_MIN_FILTER, this.gl.LINEAR);  
+    this.gl.texParameteri(this.gl.TEXTURE_2D,this. gl.TEXTURE_WRAP_S, this.gl.CLAMP_TO_EDGE);
+  }
+
+  changeColor(number) {
+    this.gl.uniform1i(this.u_Sampler, number);
+  }
+
+  draw() {
+    this.gl.clear(this.gl.COLOR_BUFFER_BIT);
+    this.gl.drawArrays(this.gl.TRIANGLE_STRIP, 0, this.vertCnt);
+  }
+
+  _initVertexBuffer() {
+
+    const vertCnt = this.vertexTexCoord.length;
   
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, vertexTexCoord, gl.STATIC_DRAW);
-
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
-  gl.enableVertexAttribArray(a_Position);
-
-  const a_TexCord = gl.getAttribLocation(gl.program, 'a_TexCord');
-  gl.vertexAttribPointer(a_TexCord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
-  gl.enableVertexAttribArray(a_TexCord);
-  return vertCnt / 4;
-};
-
-function initTextures(gl, n) {
-  const texture = gl.createTexture(); // Создать объект текстуры
-  // Получить ссылку на u_Sampler
-  const u_Sampler = gl.getUniformLocation(gl.program, 'u_Sampler');
+    this.vertexBuffer = this.gl.createBuffer();
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vertexBuffer);
+    this.gl.bufferData(
+      this.gl.ARRAY_BUFFER, 
+      this.vertexTexCoord, 
+      this.gl.STATIC_DRAW
+    );
   
-  const image = new Image(); // Создать объект изображения
-  // Зарегистрировать обработчик, вызываемый послезагрузки изображения
-  document.body.appendChild(image);
-  image.onload = function() {
-    loadTexture(gl, n, texture, u_Sampler, image);
+    this.a_Position = this.gl.getAttribLocation(this.gl.program, 'a_Position');
+
+    this.gl.vertexAttribPointer(
+      this.a_Position, 2, this.gl.FLOAT, 
+      false, this.FSIZE * 4, 0
+    );
+
+    this.gl.enableVertexAttribArray(this.a_Position);
+  
+    this.a_TexCord = this.gl.getAttribLocation(this.gl.program, 'a_TexCord');
+    
+    this.gl.vertexAttribPointer(
+      this.a_TexCord, 2, this.gl.FLOAT, 
+      false, this.FSIZE * 4, this.FSIZE * 2
+    );
+
+    this.gl.enableVertexAttribArray(this.a_TexCord);
+    return vertCnt / 4;
   };
-  // Заставить браузер загрузить изображение
-  image.src = '../resources/sky.jpg';
 }
 
-function loadTexture(gl, n, texture, u_Sampler, image) {
-  console.log(u_Sampler);
-  
-  gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1); // Повернуть ось Y изображения
-  // Выбрать текстурный слот 0
-  gl.activeTexture(gl.TEXTURE0);
-  // Указать тип объекта текстуры
-  gl.bindTexture(gl.TEXTURE_2D, texture);
-  // Определить параметры текстуры
-  gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-  // Определить изображение текстуры
-  gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGB, gl.RGB, gl.UNSIGNED_BYTE, image);
-  // Определить указатель на текстурный слот 0
-  gl.uniform1i(u_Sampler, 0);
+class Interface {
+  constructor(app, button, rgroup) {
+    this.app = app;
+    this.button = button;
+    this.rGroup = rgroup;
+
+    this.checkedRadBtn = Array.prototype.find.call(
+      this.rGroup, 
+      element => element.checked
+    );
+    
+    app.changeColor(Number(this.checkedRadBtn.value));
+    app.draw();
+
+    this.send = event => {
+      event.preventDefault();
+      
+      this.checkedRadBtn = Array.prototype.find.call(
+        this.rGroup, 
+        element => element.checked
+      );
+      
+      this.app.changeColor(Number(this.checkedRadBtn.value));
+      this.app.draw();
+    }
+    
+    button.addEventListener('click', this.send);
+  }
+    
 }
+
+const app = new App(canvas, gl, VSHADER_SOURCE, FSHADER_SOURCE);
+const int = new Interface(app, btn, rGroup);
