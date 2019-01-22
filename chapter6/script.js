4a6e88c38b71267ce3b91eebb811334e7214d7d8
Vladislav <super.tawer.822@gmail.com>
2019-01-22 23:04:44 +0300

    chapter6: page 298

--- chapter6/script.js
+++ chapter6/script.js
@@ -2,12 +2,10 @@
  const VSHADER_SOURCE =`
  attribute vec4 a_Position;
  attribute vec4 a_Color;
- uniform mat4 u_ModelMatrix;
- uniform mat4 u_ViewMatrix;
- uniform mat4 u_ProjMatrix;
+ uniform mat4 u_MVPmatrix;
  varying vec4 v_Color;
  void main() {
-  gl_Position = u_ProjMatrix * u_ViewMatrix * u_ModelMatrix * a_Position;
+  gl_Position = u_MVPmatrix * a_Position;
   v_Color = a_Color;
  }
  `;
@@ -35,91 +33,79 @@ const n = initVertexBuffers(gl);
 
 
 
-const u_ProjMatrix = gl.getUniformLocation(gl.program, 'u_ProjMatrix');
-const u_ViewMatrix = gl.getUniformLocation(gl.program, 'u_ViewMatrix');
-const u_ModelMatrix = gl.getUniformLocation(gl.program, 'u_ModelMatrix');
+const u_MVPmatrix = gl.getUniformLocation(gl.program, 'u_MVPmatrix');
 
-let viewMatrix = new Matrix4().setLookAt(0, 0, 5, 0, 0, -100, 0, 1, 0);
-let projMatrix = new Matrix4().setPerspective(30, canvas.width / canvas.height, 1, 100);
-let modelMatrix = new Matrix4().translate(0.75, 0.0, 0.0);
+let MVPmatrix = new Matrix4();
+MVPmatrix.setPerspective(30, 1, 1, 100);
+MVPmatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);;
 
-gl.uniformMatrix4fv(u_ProjMatrix, false, projMatrix.elements);
-gl.uniformMatrix4fv(u_ViewMatrix, false, viewMatrix.elements);
-gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
+gl.uniformMatrix4fv(u_MVPmatrix, false, MVPmatrix.elements);
 
-gl.drawArrays(gl.TRIANGLES, 0, n);
-
-modelMatrix.setTranslate(-0.75, 0.0, 0.0);
-gl.uniformMatrix4fv(u_ModelMatrix, false, modelMatrix.elements);
-
-gl.drawArrays(gl.TRIANGLES, 0, n);
-
-let g_eyeX = 0.0, g_eyeY = 0.0, g_eyeZ = 0.25; // Точка наблюдения
-
-document.addEventListener('keydown', event => {
-  keydown(event, gl, n, u_ModelViewMatrix, modelViewMatrix);
-});
+gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
 
 function initVertexBuffers(gl) {
-  let verticesColors = new Float32Array([// Три треугольника справа
-    0.0, 1.0, 0.0, 0.4, 0.4, 1.0, // Ближний синий треугольник
-    -0.5, -1.0, 0.0, 0.4, 0.4, 1.0,
-    0.5, -1.0, 0.0, 1.0, 0.4, 0.4,
 
-    0.0, 1.0, -2.0, 1.0, 1.0, 0.4, // Желтый треугольник в середине
-    -0.5, -1.0, -2.0, 1.0, 1.0, 0.4,
-    0.5, -1.0, -2.0, 1.0, 0.4, 0.4,
+  // Create a cube
+  //    v6----- v5
+  //   /|      /|
+  //  v1------v0|
+  //  | |     | |
+  //  | |v7---|-|v4
+  //  |/      |/
+  //  v2------v3
 
-    0.0, 1.0, -4.0, 0.4, 1.0, 0.4, // Дальний зеленый треугольник
-    -0.5, -1.0, -4.0, 0.4, 1.0, 0.4,
-    0.5, -1.0, -4.0, 1.0, 0.4, 0.4,
+    const vertices = new Float32Array([   // Vertex coordinates
+      1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+      1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+      1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+      -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+     -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+      1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+    ]);
+
+  const colors = new Float32Array([     // Colors
+    0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  // v0-v1-v2-v3 front(white)
+    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v3-v4-v5 right(white)
+    0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  // v0-v5-v6-v1 up(white)
+    1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  // v1-v6-v7-v2 left(white)
+    0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  // v7-v4-v3-v2 down(white)
+    1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0   // v4-v7-v6-v5 back(white)
   ]);
- 
-  let n = 9;
 
-  // Создать буферный объект
-  const vertexColorbuffer = gl.createBuffer();
+  const indices = new Uint8Array([       // Indices of the vertices
+    0, 1, 2,   0, 2, 3,    // front
+    4, 5, 6,   4, 6, 7,    // right
+    8, 9,10,   8,10,11,    // up
+    12,13,14,  12,14,15,    // left
+    16,17,18,  16,18,19,    // down
+    20,21,22,  20,22,23     // back
+  ]);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, vertexColorbuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, verticesColors, gl.STATIC_DRAW);
+  const indexBuffer = gl.createBuffer();
 
-  const FSIZE = verticesColors.BYTES_PER_ELEMENT;
+  initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position');
+  initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color');
 
-  const a_Position = gl.getAttribLocation(gl.program, 'a_Position');
-  gl.vertexAttribPointer(a_Position, 3, gl.FLOAT, FSIZE, FSIZE * 6, 0);
-  gl.enableVertexAttribArray(a_Position);
+  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+
+  return indices.length;
   
-  
-  const a_Color = gl.getAttribLocation(gl.program, 'a_Color');
-  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 6, FSIZE * 3);
-  gl.enableVertexAttribArray(a_Color);
-  return n;
 }
 
-function keydown(ev, gl, n, u_ModelViewMatrix, modelViewMatrix) {
-  if(ev.keyCode == 39) { // Нажата клавиша со стрелкой вправо?
-    g_eyeX += 0.01;
-  }
-  if (ev.keyCode == 37) { // Нажата клавиша со стрелкой влево?
-    g_eyeX -= 0.01;
-  } // Предотвратить ненужное перерисовывание
+function initArrayBuffer(gl, data, num, type, attribute) {
+  const buffer = gl.createBuffer();   // Create a buffer object
 
-  // switch(ev.keyCode) {
-  //   case 39: g_near += 0.01; break; // Нажата клавиша со стрелкой вправо
-  //   case 37: g_near -= 0.01; break; // Нажата клавиша со стрелкой влево
-  //   case 38: g_far += 0.01; break; // Нажата клавиша со стрелкой вверх
-  //   case 40: g_far -= 0.01; break; // Нажата клавиша со стрелкой вниз
-  //   default: return; // Предотвратить ненужное перерисовывание
-  // }
-  draw(gl, n, u_ModelViewMatrix, modelViewMatrix);
+  // Write date into the buffer object
+  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
+
+  // Assign the buffer object to the attribute variable
+  const a_attribute = gl.getAttribLocation(gl.program, attribute);
+
+  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
+  // Enable the assignment of the buffer object to the attribute variable
+  gl.enableVertexAttribArray(a_attribute);
+  return true;
 }
 
-function draw(gl, n, u_ModelViewMatrix, modelViewMatrix) {
-  modelViewMatrix.setLookAt(g_eyeX, g_eyeY, g_eyeZ, 0, 0, 0, 0, 1, 0);
-  // Передать матрицу вида в переменную u_ViewMatrix
-  
-  gl.uniformMatrix4fv(u_ModelViewMatrix, false, modelViewMatrix.elements);
-  
-  gl.clear(gl.COLOR_BUFFER_BIT); // Очистить <canvas>
-  gl.drawArrays(gl.TRIANGLES, 0, n); // Нарисовать треугольник
-}
