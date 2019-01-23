13147e19e865b5d465faf873f29b5ce0abe68d41
Vladislav <super.tawer.822@gmail.com>
2019-01-23 21:34:16 +0300

    chapter6: final app

--- chapter6/script.js
+++ chapter6/script.js
@@ -21,105 +21,140 @@
  const canvas = document.getElementById('chapter6');
  const gl = canvas.getContext('webgl');
 
- initShaders(gl, VSHADER_SOURCE, FSHADER_SOURCE);
 
- gl.clearColor(0.0, 0.0, 0.0, 1.0);
- gl.clear(gl.COLOR_BUFFER_BIT);
+ const vertices = new Float32Array([   // Vertex coordinates
+   1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
+   1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
+   1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
+  -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
+  -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
+   1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
+]);
 
- gl.enable(gl.DEPTH_TEST);
- gl.clear(gl.DEPTH_BUFFER_BIT);
+const colors = new Float32Array([     // Colors
+  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  // v0-v1-v2-v3 front(white)
+  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v3-v4-v5 right(white)
+  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  // v0-v5-v6-v1 up(white)
+  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  // v1-v6-v7-v2 left(white)
+  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  // v7-v4-v3-v2 down(white)
+  1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0   // v4-v7-v6-v5 back(white)
+]);
 
-const n = initVertexBuffers(gl);
+const indices = new Uint8Array([       // Indices of the vertices
+  0, 1, 2,   0, 2, 3,    // front
+  4, 5, 6,   4, 6, 7,    // right
+  8, 9,10,   8,10,11,    // up
+  12,13,14,  12,14,15,    // left
+  16,17,18,  16,18,19,    // down
+  20,21,22,  20,22,23     // back
+]);
 
+class App {
+  constructor(gl, vShader, fShader, vertices, colors, indexes, uniform, num = 3) {
+    this.gl = gl;
+    this.vertShader = vShader;
+    this.fragShader = fShader;
 
+    gl.clearColor(0, 0, 0, 1);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.DEPTH_BUFFER_BIT);
 
-const u_MVPmatrix = gl.getUniformLocation(gl.program, 'u_MVPmatrix');
+    initShaders(gl, vShader, fShader);
 
-let MVPmatrix = new Matrix4();
-MVPmatrix.setPerspective(30, 1, 1, 100);
-MVPmatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
+    this.vertArr = vertices;
+    this.colorsArr = colors;
+    this.indexArr = indexes;
 
-var deg = 0;
+    this.elementBuffer = this._initElementBuffer(indexes);
 
-setInterval(() => {
-  deg += 1;
-  if(deg >= 360) {
-    deg = 0;
+    const vertBufferData = this._initArrayBuffer(
+      vertices, num, 
+      gl.FLOAT, 'a_Position'
+    );
+    this.a_Position = vertBufferData.attr;
+    this.a_PosBuffer = vertBufferData.buff;
+
+    const colorsBufferData = this._initArrayBuffer(
+      colors, num, 
+      gl.FLOAT, 'a_Color'
+    );
+    this.a_Color = colorsBufferData.attr;
+    this.a_colBuffer = colorsBufferData.buff;
+
+    this._initMVPmatrix(uniform);
   }
-  MVPmatrix.setPerspective(30, 1, 1, 100);
-  MVPmatrix.lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
-  MVPmatrix.rotate(deg, 0, 0);
-  gl.uniformMatrix4fv(u_MVPmatrix, false, MVPmatrix.elements);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
-}, 100);
 
+  _initElementBuffer(data) {
+    const gl = this.gl;
 
-gl.drawElements(gl.TRIANGLES, n, gl.UNSIGNED_BYTE, 0);
+    const indexBuffer = gl.createBuffer();
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, data, gl.STATIC_DRAW);
 
-function initVertexBuffers(gl) {
+    return indexBuffer;
+  }
 
-  // Create a cube
-  //    v6----- v5
-  //   /|      /|
-  //  v1------v0|
-  //  | |     | |
-  //  | |v7---|-|v4
-  //  |/      |/
-  //  v2------v3
+  _initArrayBuffer(data, num, type, attribute) {
+    const gl = this.gl;
 
-    const vertices = new Float32Array([   // Vertex coordinates
-      1.0, 1.0, 1.0,  -1.0, 1.0, 1.0,  -1.0,-1.0, 1.0,   1.0,-1.0, 1.0,  // v0-v1-v2-v3 front
-      1.0, 1.0, 1.0,   1.0,-1.0, 1.0,   1.0,-1.0,-1.0,   1.0, 1.0,-1.0,  // v0-v3-v4-v5 right
-      1.0, 1.0, 1.0,   1.0, 1.0,-1.0,  -1.0, 1.0,-1.0,  -1.0, 1.0, 1.0,  // v0-v5-v6-v1 up
-      -1.0, 1.0, 1.0,  -1.0, 1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0,-1.0, 1.0,  // v1-v6-v7-v2 left
-     -1.0,-1.0,-1.0,   1.0,-1.0,-1.0,   1.0,-1.0, 1.0,  -1.0,-1.0, 1.0,  // v7-v4-v3-v2 down
-      1.0,-1.0,-1.0,  -1.0,-1.0,-1.0,  -1.0, 1.0,-1.0,   1.0, 1.0,-1.0   // v4-v7-v6-v5 back
-    ]);
+    const buffer = this.gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
 
-  const colors = new Float32Array([     // Colors
-    0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  0.0, 0.0, 1.0,  // v0-v1-v2-v3 front(white)
-    1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  1.0, 0.0, 0.0,  // v0-v3-v4-v5 right(white)
-    0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  0.0, 1.0, 0.0,  // v0-v5-v6-v1 up(white)
-    1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  1.0, 1.0, 0.0,  // v1-v6-v7-v2 left(white)
-    0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  0.0, 1.0, 1.0,  // v7-v4-v3-v2 down(white)
-    1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0,  1.0, 0.0, 1.0   // v4-v7-v6-v5 back(white)
-  ]);
+    const a_Attr = gl.getAttribLocation(gl.program, attribute);
+    gl.vertexAttribPointer(a_Attr, num, type, false, 0, 0);
+    gl.enableVertexAttribArray(a_Attr);
 
-  const indices = new Uint8Array([       // Indices of the vertices
-    0, 1, 2,   0, 2, 3,    // front
-    4, 5, 6,   4, 6, 7,    // right
-    8, 9,10,   8,10,11,    // up
-    12,13,14,  12,14,15,    // left
-    16,17,18,  16,18,19,    // down
-    20,21,22,  20,22,23     // back
-  ]);
+    return { buff: buffer, attr: a_Attr };
+  }
 
-  const indexBuffer = gl.createBuffer();
+  _initMVPmatrix(uniform) {
+    const gl = this.gl;
 
-  initArrayBuffer(gl, vertices, 3, gl.FLOAT, 'a_Position');
-  initArrayBuffer(gl, colors, 3, gl.FLOAT, 'a_Color');
+    this.u_MVPmatrix = gl.getUniformLocation(gl.program, uniform);
 
-  gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, indexBuffer);
-  gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, indices, gl.STATIC_DRAW);
+    this.MVPmatrix = new Matrix4()
+    .setPerspective(30, 1, 1, 100)
+    .lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0);
 
-  return indices.length;
-  
+    gl.uniformMatrix4fv(this.u_MVPmatrix, false, this.MVPmatrix.elements);
+  }
+
+  draw() {
+    const gl = this.gl; 
+
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.DEPTH_BUFFER_BIT);
+    gl.drawElements(gl.TRIANGLES, this.indexArr.length, gl.UNSIGNED_BYTE, 0);
+  }
+
+   rotate() {
+    const self = this;
+
+    let deg = 0;
+
+    function _rotateCallback() {
+      deg = deg >= 360 ? 0 : ++deg;
+
+      self.MVPmatrix
+      .setPerspective(30, 1, 1, 100)
+      .lookAt(3, 3, 7, 0, 0, 0, 0, 1, 0)
+      .rotate(deg, 0, 0);
+      
+      self.gl.uniformMatrix4fv(self.u_MVPmatrix, false, self.MVPmatrix.elements);
+      self.draw();
+
+      requestAnimationFrame(_rotateCallback);
+    }
+
+    requestAnimationFrame(_rotateCallback);
+  }
 }
 
-function initArrayBuffer(gl, data, num, type, attribute) {
-  const buffer = gl.createBuffer();   // Create a buffer object
+const app = new App(
+  gl, VSHADER_SOURCE, FSHADER_SOURCE, 
+  vertices, colors, indices, 'u_MVPmatrix', 3
+);
 
-  // Write date into the buffer object
-  gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
-  gl.bufferData(gl.ARRAY_BUFFER, data, gl.STATIC_DRAW);
-
-  // Assign the buffer object to the attribute variable
-  const a_attribute = gl.getAttribLocation(gl.program, attribute);
-
-  gl.vertexAttribPointer(a_attribute, num, type, false, 0, 0);
-  // Enable the assignment of the buffer object to the attribute variable
-  gl.enableVertexAttribArray(a_attribute);
-  return true;
-}
-
+app.rotate();
