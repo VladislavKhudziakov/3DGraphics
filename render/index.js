e575dbe10b8f37a1fc4c0e10bdfbd45f4a189582
Vladislav Khudziakov <vladislav.khudiakov@banuba.com>
2019-02-21 18:49:30 +0300

    render: add camera

--- render/index.js
+++ render/index.js
@@ -3,6 +3,7 @@
 // import {main} from "./main";
 
 loadFiles('./vertex.vert', './fragment.frag', './model.json').then(main);
+
 function getFile(url) {
   return new Promise((resolve, reject) => {
 
@@ -26,8 +27,6 @@ async function loadFiles(vShaderUrl, fShaderUrl, modelUrl) {
   const model = await getFile(modelUrl);
   return [vShader, fShader, JSON.parse(model)];
 }
-
-loadFiles('./vertex.vert', './fragment.frag', './model.json').then(main);
 
 
 function main(filesSource) {
@@ -54,12 +53,14 @@ function main(filesSource) {
    * |
    * ↓
    */
-  const left = 0;
-  const top = 0;
-  const right = gl.canvas.clientWidth;
-  const bottom = gl.canvas.clientWidth;
-  const near = 400;
-  const far = -400;
+  // const left = 0;
+  // const top = 0;
+  // const right = gl.canvas.clientWidth;
+  // const bottom = gl.canvas.clientWidth;
+  // const near = 400;
+  // const far = -400;
+
+ 
 
   gl.useProgram(gl_Program);
 
@@ -70,25 +71,74 @@ function main(filesSource) {
 
   const a_Position = initArrayBuffer(gl, 'a_Position', gl_Program, new Float32Array(mdl.vertices), 3);
   const a_Color = initArrayBuffer(gl, 'a_Color', gl_Program, new Float32Array(mdl.colors), 3);
+  // const a_Normal = initArrayBuffer(gl, 'a_Normal', new Float32Array([]))
 
-  let proj = setOrtho(right, left, top, bottom, far, near);
-  
-  let model = mul(setSingle(), setTranslation(300, 300, 0));
-  
-  
-  model = mul(model, setScaling(1, 1, 1));
-  
-  model = mul(model, setRotationX(10));
+  const u_mvp = gl.getUniformLocation(gl_Program, 'u_mvp');
+  let currAngleY = 0;
+  let currAngleX = 0;
+  document.body.addEventListener('keydown', (event) => {
+    if (event.keyCode == 65) {
+      currAngleY--;
+    } else if(event.keyCode == 68) {
+      currAngleY++;
+    } else if(event.keyCode == 87) {
+      currAngleX++;
+    } else if(event.keyCode == 83) {
+      currAngleX--;
+    }
+    // console.log(event.keyCode);
+    draw();
+  });
+  draw();
+  function draw() {
+    gl.clearColor(0, 0, 0, 1);
+    gl.enable(gl.DEPTH_TEST);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    gl.clear(gl.DEPTH_BUFFER_BIT);
+      // let proj = setOrtho(right, left, top, bottom, far, near);
+    const fov = 60;
+    const aspect = canvas.clientWidth / canvas.clientHeight;
+    const near = 1;
+    const far = 2000;
 
-  model = mul(model, setRotationY(20));
-  model = mul(model, setRotationZ(0));
-  
-  let mp = mul(proj, model);
-  
-  const u_mv = gl.getUniformLocation(gl_Program, 'u_mv');
-  gl.uniformMatrix4fv(u_mv, false, new Float32Array(mp));
-  
-  gl.drawArrays(primitiveType, drawOffset, mdl.vertices.length / 3);
+    let proj = setPerspective(fov, aspect, near, far);
+
+    const fCount = 5;
+    const fRadius = 200;
+    
+    let view = setRotationY(currAngleY);
+    view = mul(view, setRotationX(currAngleX));
+    // view = mul(view, setRotationZ(180));
+    view = mul(view, setTranslation(0, 0, fRadius * 3));
+    
+    const camPos = [
+      view[12],
+      view[13],
+      view[14]
+    ];
+
+    const up = [0, 1, 0];
+    const fPos = [fRadius, 0, 0];
+
+    let camera = lookAt(camPos, fPos, up);
+    camera = setInverse(camera);
+    const vp = mul(proj, camera);
+
+    for (let i = 0; i < fCount; i++) {
+      const angle = (i * Math.PI * 2) / fCount;
+      const x = Math.cos(angle) * fRadius;
+      const y = Math.sin(angle) * fRadius;
+      
+      let model = setRotationY(0);
+      // model = mul(model, setRotationZ(180));
+      model = mul(model, setTranslation(x, 0, y));
+      // model = mul(model, s);
+      const mvp = mul(vp, model);
+      // console.log(mvp);
+      gl.uniformMatrix4fv(u_mvp, false, new Float32Array(mvp));
+      gl.drawArrays(primitiveType, drawOffset, mdl.vertices.length / 3);
+    }
+  }
 }
 
 
@@ -235,6 +285,23 @@ function setOrtho(right, left, top, bottom, far, near) {
   return ortho;
 }
 
+
+function setPerspective(fov, aspect, near, far) {
+  const fovRad = (Math.PI * fov) / 180;
+  const tn = Math.tan(fovRad / 2);
+  const sX = 1 / (aspect * tn);
+  const sY = 1 / tn;
+  const sZ = -((far + near) / (far - near));
+  const tZ = -((2 * far * near) / (far - near));
+
+  return [
+    sX, 0,  0,  0,
+    0, sY,  0,  0,
+    0,  0, sZ, -1,
+    0,  0, tZ,  0
+  ];
+}
+
 function mul(A, B) {
   let i, e, a, b, ai0, ai1, ai2, ai3;
   
@@ -260,4 +327,185 @@ function mul(A, B) {
   }
   
   return e;
+};
+
+function subtractVectors(a, b) {
+  return [a[0] - b[0], a[1] - b[1], a[2] - b[2]];
+}
+
+function normalize(v) {
+  var length = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+  // make sure we don't divide by 0.
+  if (length > 0.00001) {
+    return [v[0] / length, v[1] / length, v[2] / length];
+  } else {
+    return [0, 0, 0];
+  }
+}
+
+function cross(a, b) {
+  return [a[1] * b[2] - a[2] * b[1],
+          a[2] * b[0] - a[0] * b[2],
+          a[0] * b[1] - a[1] * b[0]];
+}
+
+
+
+  function lookAt(cameraPosition, target, up) {
+    var zAxis = normalize(
+        subtractVectors(cameraPosition, target));
+    var xAxis = normalize(cross(up, zAxis));
+    var yAxis = normalize(cross(zAxis, xAxis));
+
+    return [
+       xAxis[0], xAxis[1], xAxis[2], 0,
+       yAxis[0], yAxis[1], yAxis[2], 0,
+       zAxis[0], zAxis[1], zAxis[2], 0,
+       cameraPosition[0],
+       cameraPosition[1],
+       cameraPosition[2],
+       1,
+    ];
+  };
+
+// function cross(v1, v2) {
+//   /**
+//    *   i     j     k
+//    * v1[0] v1[1] v1[2]
+//    * v2[0] v2[1] v2[2]
+//    */
+
+//    return [
+//      v1[1] * v2[2] - v2[1] * v2[2],
+//      v1[0] * v2[2] - v2[0] * v1[2],
+//      -(v1[0] * v2[1] - v2[0] * v1[1]) 
+//     ];
+// };
+
+
+// function substract(v1, v2) {
+//   return[
+//     v1[0] - v2[0],
+//     v1[1] - v2[1],
+//     v1[2] - v2[2]
+//   ];
+// };
+
+// function normalize(v) {
+//   const len = Math.sqrt(v[0] * v[0] + v[1] * v[1] + v[2] * v[2]);
+
+//   if(len > 0.00001) {
+    
+//     return[
+//       v[0] / len,
+//       v[1] / len,
+//       v[2] / len
+//     ];
+//   } else {
+//     return [
+//       0,
+//       0,
+//       0
+//     ];
+//   }
+// };
+
+
+// function setLookAt(camPos, targetPos, up) {
+//   let zPos = normalize(substract(targetPos, camPos));
+//   let xPos = normalize(cross(up, zPos));
+//   let yPos = normalize(cross(zPos, xPos));
+  
+//   return [
+//       xPos[0],   xPos[1],   xPos[2],   0,
+//       yPos[0],   yPos[1],   yPos[2],   0,
+//       zPos[0],   zPos[1],   zPos[2],   0,
+//     camPos[0], camPos[1], camPos[2],   1
+//   ];
+// };
+
+
+function setInverse(m) {
+  var m00 = m[0 * 4 + 0];
+  var m01 = m[0 * 4 + 1];
+  var m02 = m[0 * 4 + 2];
+  var m03 = m[0 * 4 + 3];
+  var m10 = m[1 * 4 + 0];
+  var m11 = m[1 * 4 + 1];
+  var m12 = m[1 * 4 + 2];
+  var m13 = m[1 * 4 + 3];
+  var m20 = m[2 * 4 + 0];
+  var m21 = m[2 * 4 + 1];
+  var m22 = m[2 * 4 + 2];
+  var m23 = m[2 * 4 + 3];
+  var m30 = m[3 * 4 + 0];
+  var m31 = m[3 * 4 + 1];
+  var m32 = m[3 * 4 + 2];
+  var m33 = m[3 * 4 + 3];
+  var tmp_0  = m22 * m33;
+  var tmp_1  = m32 * m23;
+  var tmp_2  = m12 * m33;
+  var tmp_3  = m32 * m13;
+  var tmp_4  = m12 * m23;
+  var tmp_5  = m22 * m13;
+  var tmp_6  = m02 * m33;
+  var tmp_7  = m32 * m03;
+  var tmp_8  = m02 * m23;
+  var tmp_9  = m22 * m03;
+  var tmp_10 = m02 * m13;
+  var tmp_11 = m12 * m03;
+  var tmp_12 = m20 * m31;
+  var tmp_13 = m30 * m21;
+  var tmp_14 = m10 * m31;
+  var tmp_15 = m30 * m11;
+  var tmp_16 = m10 * m21;
+  var tmp_17 = m20 * m11;
+  var tmp_18 = m00 * m31;
+  var tmp_19 = m30 * m01;
+  var tmp_20 = m00 * m21;
+  var tmp_21 = m20 * m01;
+  var tmp_22 = m00 * m11;
+  var tmp_23 = m10 * m01;
+
+  var t0 = (tmp_0 * m11 + tmp_3 * m21 + tmp_4 * m31) -
+      (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+  var t1 = (tmp_1 * m01 + tmp_6 * m21 + tmp_9 * m31) -
+      (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+  var t2 = (tmp_2 * m01 + tmp_7 * m11 + tmp_10 * m31) -
+      (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+  var t3 = (tmp_5 * m01 + tmp_8 * m11 + tmp_11 * m21) -
+      (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+  var d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+  return [
+    d * t0,
+    d * t1,
+    d * t2,
+    d * t3,
+    d * ((tmp_1 * m10 + tmp_2 * m20 + tmp_5 * m30) -
+          (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30)),
+    d * ((tmp_0 * m00 + tmp_7 * m20 + tmp_8 * m30) -
+          (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30)),
+    d * ((tmp_3 * m00 + tmp_6 * m10 + tmp_11 * m30) -
+          (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30)),
+    d * ((tmp_4 * m00 + tmp_9 * m10 + tmp_10 * m20) -
+          (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20)),
+    d * ((tmp_12 * m13 + tmp_15 * m23 + tmp_16 * m33) -
+          (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33)),
+    d * ((tmp_13 * m03 + tmp_18 * m23 + tmp_21 * m33) -
+          (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33)),
+    d * ((tmp_14 * m03 + tmp_19 * m13 + tmp_22 * m33) -
+          (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33)),
+    d * ((tmp_17 * m03 + tmp_20 * m13 + tmp_23 * m23) -
+          (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23)),
+    d * ((tmp_14 * m22 + tmp_17 * m32 + tmp_13 * m12) -
+          (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22)),
+    d * ((tmp_20 * m32 + tmp_12 * m02 + tmp_19 * m22) -
+          (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02)),
+    d * ((tmp_18 * m12 + tmp_23 * m32 + tmp_15 * m02) -
+          (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12)),
+    d * ((tmp_22 * m22 + tmp_16 * m02 + tmp_21 * m12) -
+          (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02))
+  ];
 };
--- render/vertex.vert
+++ render/vertex.vert
@@ -1,13 +1,18 @@
 attribute vec4 a_Position;
 attribute vec4 a_Color;
+attribute vec4 a_Normal;
 
-uniform mat4 u_mv;
+uniform mat4 u_mvp;
+// uniform float u_zFactor;
 
 varying vec4 v_Color;
 
 void main()
 {
-  gl_Position = u_mv * a_Position;
+  // vec4 vpos = u_mv * a_Position;
+  
+  // float zCoeff = 1. + vpos.z * u_zFactor;
+  gl_Position = u_mvp * a_Position;
 
   v_Color = a_Color;
 }
